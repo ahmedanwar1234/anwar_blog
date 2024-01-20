efac60ffc761722318b8cbd8fda68aa78e7990c6
ahmedanwar123 <ahmedalprenses@gmail.com>
2024-01-20 03:22:28 +0200

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,10 @@ function App() {
 
   return (
     <>
-<div className=' text-red-400  font-bold text-3xl rounded-full bg-blue-50 w-fit text-center  mx-auto'> anwar</div>
+<div className=' text-red-400  font-bold text-3xl rounded-full bg-blue-50 w-fit text-center  mx-auto'> 
+anwar
+
+</div>
 
     </>
   )
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,53 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import App from './App';
+import ContactPage from './routes/ContactPage';
+import About from './routes/About';
+import SignIn from './routes/SignIn';
+import SignUp from './routes/SignUp';
+import Dashboard from './routes/Dashboard';
+import Projects from './routes/Projects';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path:'/contact',
+    element:<ContactPage/>
+  },
+  {
+    path:'/about',
+    element:<About/>
+  },
+  {
+    path:'/signin',
+    element:<SignIn/>
+  },
+  {
+    path:'/signup',
+    element:<SignUp/>
+  },
+  {
+    path:'/dashboard',
+    element:<Dashboard/>
+  },
+  {
+    path:'/projects',
+    element:<Projects/>,
+    errorElement:<div>error</div>,
+ 
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
