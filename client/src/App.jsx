269160f7c9710a5a145e84b92affa25ede0605b9
ahmedanwar123 <ahmedalprenses@gmail.com>
2024-01-20 02:52:47 +0200

    install react js and tailwind css adn create the first template

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+<div className=' text-red-400  font-bold text-3xl rounded-full bg-blue-50 w-fit text-center  mx-auto'> anwar</div>
+
+    </>
+  )
+}
+
+export default App
