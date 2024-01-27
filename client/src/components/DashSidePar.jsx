73e6e78048ee9a5bba45d7021800b6fdba4e579f
ahmedanwar123 <ahmedalprenses@gmail.com>
2024-01-27 17:29:57 +0200

    complete user image upload functionlaty and upload any thing in Dashbpard Profile

--- client/src/components/DashSidePar.jsx
+++ client/src/components/DashSidePar.jsx
@@ -1,32 +1,40 @@
 import { Sidebar } from 'flowbite-react'
 import  { useEffect, useState } from 'react'
 import { HiArrowSmRight, HiUser } from 'react-icons/hi'
+import { useDispatch } from 'react-redux';
 import { useLocation } from 'react-router';
 import { Link } from 'react-router-dom';
+import { signoutSuccess } from '../redux/user/userSlice';
 
 const DashSidePar = () => {
-    const location=useLocation();
-    const[tab,setTab]=useState('');
-    useEffect(()=>{
-      const urlParams=new URLSearchParams(location.search)
-      const tabFromUrl=urlParams.get('tab')
-      if(tabFromUrl){
-  
-        setTab(tabFromUrl)
-      }
-    },[location.search,setTab])
+  const location=useLocation();
+  const[tab,setTab]=useState('');
+  const disbatch=useDispatch()
+  useEffect(()=>{
+    const urlParams=new URLSearchParams(location.search)
+    const tabFromUrl=urlParams.get('tab')
+    if(tabFromUrl){
+      
+      setTab(tabFromUrl)
+    }
+  },[location.search,setTab])
+
+
+  const handlSignOut=()=>{
+disbatch(signoutSuccess())
+  }
   
   return (
     <Sidebar className=' w-full md:w-56'>
 <Sidebar.Items>
 <Sidebar.ItemGroup>
 <Link to={'/dashboard?tab=profile'}>
-<Sidebar.Item active={tab==='profile'} icon={HiUser} label={"User"} labelColor="dark" >
+<Sidebar.Item as="div" active={tab==='profile'} icon={HiUser} label={"User"} labelColor="dark" >
 Profile
 </Sidebar.Item>
 </Link>
 <Sidebar.Item active icon={HiArrowSmRight} className="cursor-pointer">
-Sign Out
+<button className='' onClick={handlSignOut}>Sign Out</button>
 </Sidebar.Item>
 
 
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,53 @@
-import { Avatar, Button, Dropdown, Navbar, NavbarToggle, TextInput } from 'flowbite-react'
+import { Avatar, Button, Dropdown, Navbar, NavbarToggle, TextInput, Toast } from 'flowbite-react'
 import { Link ,useLocation} from 'react-router-dom'
 import {AiOutlineSearch}from 'react-icons/ai'
 import {FaMoon, FaSun}from 'react-icons/fa'
 import {useDispatch, useSelector}from 'react-redux'
 import {toggleTheme}from '../redux/theme/themeSlice'
+import { signoutSuccess } from '../redux/user/userSlice'
+import { useCallback, useState } from 'react'
+import { HiCheck } from 'react-icons/hi'
 const Header = () => {
+  const [truee,setTruee]=useState(false)
+  const [showToast,setShowToast]=useState(false)
+
     const path=useLocation().pathname;
     const {currentUser}=useSelector(state=>state.user);
     const {theme}=useSelector(state=>state.theme)
     const disbatch=useDispatch()
+
+    const handlSignOut=useCallback(()=>{
+      disbatch(signoutSuccess())
+      setTruee(false)
+        setShowToast(true)
+ 
+        },[disbatch])
   return (
     <Navbar className=' border-b-2'>
+      <div className={`${truee?"fixed top-[50%] left-1/2":"hidden"} translate-x-[-50%] translate-y-[-50%]`}>
+<div className=' flex flex-col justify-center items-center gap-5 bg-red-500 p-5 rounded-xl'>
+  <h1 className='font-semibold text-lg'>are you sure to sign out</h1>
+
+<div className=' felx gap-8 '>
+<button onClick={handlSignOut} className=' font-semibold text-lg mr-10 '>yes</button>
+  <button onClick={()=>setTruee(!truee)} className=' font-semibold text-lg'>no</button>
+</div>
+
+</div>
+      </div>
+      {
+      showToast&&
+      <div className='fixed bottom-10 right-10'>
+    
+      <Toast>
+        <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-green-100 text-green-500 dark:bg-green-600 dark:text-green-200">
+          <HiCheck className="h-5 w-5" />
+        </div>
+        <div className="ml-3 text-sm font-normal">Successfully Sign Out</div>
+       
+      </Toast>
+      </div>
+      }
 <Link className=' self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white' to={'/'} ><span className='px-2 py-1  bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Sahands </span>Blog</Link>
 
 <form action="">
@@ -47,7 +84,7 @@ theme==='dark'?( <FaMoon />):(<FaSun/>)
 <Dropdown.Item>Profile</Dropdown.Item>
 </Link>
 <Dropdown.Divider/>
-<Dropdown.Item>Sign Out</Dropdown.Item>
+<Dropdown.Item onClick={()=>setTruee(!truee)}>Sign Out</Dropdown.Item>
 </Dropdown>
 ):(
   <Link to={'/sign-in'}>
