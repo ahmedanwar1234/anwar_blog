3ad8f8533e4783564ee2e1bcf9c2d36cedb16051
ahmedanwar123 <ahmedalprenses@gmail.com>
2024-01-30 01:05:43 +0200

    add getUsers api route

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,6 +1,6 @@
 import User from '../models/user.model.js'
 import express from 'express'
-import { getUser,updateUser,deleteUser,signout} from '../controllers/user.controller.js'
+import { getUser,updateUser,deleteUser,signout,getUsers} from '../controllers/user.controller.js'
 import { verifyToken } from '../utilis/verifyUser.js'
 
 const route=express.Router()
@@ -10,6 +10,7 @@ const route=express.Router()
  route.put('/update/:userId',verifyToken,updateUser)
  route.delete('/delete/:userId',verifyToken,deleteUser)
  route.post('/signout',signout)
+ route.get('/getusers',verifyToken,getUsers)
 
 
 export default route
